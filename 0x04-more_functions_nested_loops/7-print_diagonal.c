c2da5b04b0050e619fd6aece4a5207cf4386816f
“Chibuike <ositachibike@gmail.com>
2022-09-15 15:17:50 -0700

    Task eleven

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,18 +9,21 @@
 
 void print_diagonal(int n)
 {
-	int postn, space;
+	int i = 0, ii;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
+	while (i < n && n > 0)
 	{
-		for (postn = 1; postn <= n; ++postn)
+		ii = 0;
+		while (ii < i)
 		{
-			for (space = 1; space <= postn; ++space)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
-			_putchar('\n');
+			_putchar(' ');
+			ii++;
 		}
+
+		_putchar('\\');
+		_putchar('\n');
+		i++;
 	}
+	if (i == 0)
+		_putchar('\n');
 }
