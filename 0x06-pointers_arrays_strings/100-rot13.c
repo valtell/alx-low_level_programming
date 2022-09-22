d703a1570666eb3eb322a642452bc93ee0e4a8f7
“Chibuike <ositachibike@gmail.com>
2022-09-22 10:45:01 -0700

    Task 88

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,31 @@
 #include "main.h"
-
 /**
- * rot13 - encodes a string using rot13
- * @s: string to encode 
+ *rot13 - encodes strings using rot13.
+ *@s: pointer to string.
  *
- * Return: address of s
-*/
-
+ *Return: pointer to encoded string.
+ */
 char *rot13(char *s)
 {
-	int i, j;
-	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int stringCount, rotation;
+	char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		     'Z'};
+	char r2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		     'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		     'M'};
 
-	for (i = 0; *(s + i); i++)
+	for (stringCount = 0; s[stringCount] != '\0'; stringCount++)
 	{
-		for (j = 0; j < 52; j++)
+		for (rotation = 0; rotation < 53; rotation++)
 		{
-			if (a[j] == *(s + i))
+			if (r1[rotation] == s[stringCount])
 			{
-				*(s + i) = b[j];
+				s[stringCount] = r2[rotation];
 				break;
 			}
 		}
